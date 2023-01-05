c65fe5096c54f4af9b0596d5b7713d8d5fec5726
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-01-04 23:26:28 -0500

    update
    Finished Header section and abstracted to its own component. Next is to implement form and meme image into a new component.

--- src/App.js
+++ src/App.js
@@ -1,24 +1,45 @@
-import logo from './logo.svg';
-import './App.css';
+import "./App.css";
+import Header from "./Header"
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p className="text-red-500">
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="card">
+        <Header />
+        <main>
+          <form class="w-full max-w-lg">
+            <div class="flex flex-wrap">
+              <div class="w-1/2">
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-red-500 rounded py-3 px-4 mb-3 leading-tight focus:outline-none focus:bg-white"
+                  id="grid-first-name"
+                  type="text"
+                  placeholder="Jane"
+                />
+                {/* <p class="text-red-500 text-xs italic">
+                  Please fill out this field.
+                </p> */}
+              </div>
+
+              <div class="">
+                <input
+                  class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
+                  id="grid-last-name"
+                  type="text"
+                  placeholder="Doe"
+                />
+              </div>
+            </div>
+
+            <div class="flex flex-wrap -mx-3 mb-6">
+              <div class="w-full px-3">
+                <button>Get a new meme image 🖼️</button>
+              </div>
+            </div>
+          </form>
+        </main>
+      </div>
+    </>
   );
 }
 
