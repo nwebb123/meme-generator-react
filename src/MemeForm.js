19e6097b6735a55f25c947db041ccfa5f5afa4da
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-01-08 13:46:50 -0500

    Update MemeForm.js
    Finished basic styling for small and md-lg screen sizes for inputs and button

--- src/MemeForm.js
+++ src/MemeForm.js
@@ -2,10 +2,9 @@ import React from "react";
 
 function MemeForm() {
   return (
-    <form class="w-full">
-      <div class="flex flex-wrap justify-center mx-8">
-
-        <div class="w-full m-3 md:1/2 ">
+    <main>
+      <form class="w-full">
+        <div className="grid md:grid-cols-2 m-4 gap-2">
           <input
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-first-name"
@@ -15,9 +14,6 @@ function MemeForm() {
           {/* <p class="text-red-500 text-xs italic">
                   Please fill out this field.
                 </p> */}
-        </div>
-
-        <div class="w-full m-3">
           <input
             class="appearance-none block w-full bg-gray-200 text-gray-700 border border-gray-200 rounded py-3 px-4 leading-tight focus:outline-none focus:bg-white focus:border-gray-500"
             id="grid-last-name"
@@ -25,15 +21,14 @@ function MemeForm() {
             placeholder="and take my money"
           />
         </div>
-
-      </div>
-
-      <div class="flex justify-center mb-6">
-        <button className="w-1/2 text-center font-medium text-md text-white justify-center p-4 m-3 rounded-md bg-gradient-to-br from-purple-800 to-purple-400">
-          Get a new meme image 🖼️
-        </button>
-      </div>
-    </form>
+        <div className="btn mx-auto flex justify-center">
+       
+          <button className="text-center font-medium text-md  text-white p-4 m-3 rounded-md bg-gradient-to-br from-purple-800 to-purple-400">
+            Get a new meme image 🖼️
+          </button>
+        </div>
+      </form>
+    </main>
   );
 }
 
