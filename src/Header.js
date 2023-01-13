1ca21be7902850b0829ce4f1c1d85aabfd5bcf31
nwebb123 <68877910+nwebb123@users.noreply.github.com>
2023-01-12 19:40:50 -0500

    Update Header.js
    small update

--- src/Header.js
+++ src/Header.js
@@ -4,7 +4,7 @@ import logo from "../src/assets/title-logo.png";
 function Header() {
   return (
     <header className="px-5 py-8 bg-gradient-to-br from-purple-800 to-purple-400">
-      <img src={logo} alt="Meme Generator Logo" className="absolute h-8" />
+      <img src={logo} alt="Meme Generator Logo " className="absolute h-8" />
       <div className="flex justify-end">
         <h4 className="text-white pr-5">React Course - Project 3</h4>
       </div>
